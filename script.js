8b5982d87b5efda7f275fd3e7450ec35d3f59876
iamxojiakbar <xtursunmahammatov@gmail.com>
2023-06-09 12:56:19 +0300

    hello github.

---
+++ script.js
@@ -0,0 +1,48 @@
+// console.log ----
+
+//    VARIABLES:
+
+// var, let, consta
+
+//
+// NaN - not a number raqam bolmagan ma'lumotlarni ustida arifmetik amal bajarilsa, NaN degan qiymat qaytaradi. + plus bn tekshirilsa boladi.
+
+// HOME WORK
+
+// JS HAS 8 DATA TYPES: STRING, NUMBER, BOOLEAN, bIGINT, UNDEFINED, SYMBOL, OBJECT, NULL.
+
+// let length = 16;
+// let width = 2.3;
+
+// let color = "Yellow";
+// let name = "James";
+
+// let x = true;
+// let y = false;
+
+// //  object:
+// const person = { firstName: "James", lastName: "Mcking" };
+
+// // type of
+// let r;
+// r = 5;
+// r = "james";
+
+// //  bigint
+// let z = 900131414004114n;
+// let b = 123456789n;
+// let d = z * b;
+
+// // symbol
+
+// let id1 = Symbol("ID");
+// let id2 = Symbol("ID2");
+
+
+// let h = "" + 1 + 0;
+// console.log(h);
+
+
+
+
+
